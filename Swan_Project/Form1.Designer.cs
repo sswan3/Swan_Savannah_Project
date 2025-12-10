0ed05e1f5738096a8388fc18fe493a16e15512c5
sswan3 <savannahhswan@gmail.com>
2025-12-10 11:50:25 -0800

    Add project files.

---
+++ Swan_Project/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Swan_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            add = new Button();
+            subtract = new Button();
+            divide = new Button();
+            multiply = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(56, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(200, 74);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // add
+            // 
+            add.Location = new Point(539, 89);
+            add.Name = "add";
+            add.Size = new Size(94, 29);
+            add.TabIndex = 2;
+            add.Text = "add";
+            add.UseVisualStyleBackColor = true;
+            add.Click += add_Click;
+            // 
+            // subtract
+            // 
+            subtract.Location = new Point(549, 134);
+            subtract.Name = "subtract";
+            subtract.Size = new Size(94, 29);
+            subtract.TabIndex = 3;
+            subtract.Text = "subtract";
+            subtract.UseVisualStyleBackColor = true;
+            subtract.Click += subtract_Click;
+            // 
+            // divide
+            // 
+            divide.Location = new Point(559, 199);
+            divide.Name = "divide";
+            divide.Size = new Size(94, 29);
+            divide.TabIndex = 4;
+            divide.Text = "divide";
+            divide.UseVisualStyleBackColor = true;
+            divide.Click += divide_Click;
+            // 
+            // multiply
+            // 
+            multiply.Location = new Point(559, 251);
+            multiply.Name = "multiply";
+            multiply.Size = new Size(94, 29);
+            multiply.TabIndex = 5;
+            multiply.Text = "multiply";
+            multiply.UseVisualStyleBackColor = true;
+            multiply.Click += multiply_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(multiply);
+            Controls.Add(divide);
+            Controls.Add(subtract);
+            Controls.Add(add);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button add;
+        private Button subtract;
+        private Button divide;
+        private Button multiply;
+    }
+}
---
+++ Swan_Project/Form1.cs
@@ -0,0 +1,107 @@
+namespace Swan_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            string userinput = textBox1.Text;
+            string userinput2 = textBox2.Text;
+
+            double input = double.Parse(userinput);
+            double input2 = double.Parse(userinput2);
+
+            Calculate calc = new Calculate(input, input2);
+            double sum = calc.add(input, input2);
+            MessageBox.Show(sum.ToString());
+
+
+        }
+
+        private void subtract_Click(object sender, EventArgs e)
+        {
+            string userinput = textBox1.Text;
+            string userinput2 = textBox2.Text;
+
+            double input = double.Parse(userinput);
+            double input2 = double.Parse(userinput2);
+
+            Calculate calc = new Calculate(input, input2);
+            double difference = calc.subtract(input, input2);
+            MessageBox.Show(difference.ToString());
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            string userinput = textBox1.Text;
+            string userinput2 = textBox2.Text;
+
+            double input = double.Parse(userinput);
+            double input2 = double.Parse(userinput2);
+
+            Calculate calc = new Calculate(input, input2);
+            double divide = calc.divide(input, input2);
+            MessageBox.Show(divide.ToString());
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            string userinput = textBox1.Text;
+            string userinput2 = textBox2.Text;
+
+            double input = double.Parse(userinput);
+            double input2 = double.Parse(userinput2);
+
+            Calculate calc = new Calculate(input, input2);
+            double product = calc.multiply(input, input2);
+            MessageBox.Show(product.ToString());
+        }
+
+    }
+    class Calculate : Calculator
+    {
+        private double num1 { get; set; }
+        private double num2 { get; set; }
+
+        public Calculate(double num1, double num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+    }
+    class Calculator
+    {
+
+        public double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+    }
+
+}
